83f89f2616f644ce8008a1f3d3dc253cef690332
guterc2 <a.igorgamerbr@gmail.com>
2026-04-10 22:16:37 -0300

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-function salvar(lista) {
-    localStorage.setItem("agendamentos", JSON.stringify(lista));
-}
-
-function carregar() {
-    let dados = localStorage.getItem("agendamentos");
-    return dados ? JSON.parse(dados) : [];
-}
